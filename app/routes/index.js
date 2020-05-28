93fdcd93f0f5573da4e961e2b591afda816e9228
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-05-28 14:08:45 +0300

    first post and get

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 const noteRoutes = require('./note_routes');
 module.exports = function(app, db) {
-  noteRoutes(app, db);
+  noteRoutes(app, db.db(`notes`));
   // Тут, позже, будут и другие обработчики маршрутов 
 };
--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,26 @@
+var ObjectId = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
-    app.post('/notes', (req, res) => {
-        console.log(req.body)
-        res.send('Hello')
+  app.get('/notes/:id', (req, res) => {
+    console.log('req.params.id', req.params.id);
+    const id = req.params.id;
+    const details = { '_id': new ObjectId(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      } 
     });
+  });
+  app.post('/notes', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
 };
