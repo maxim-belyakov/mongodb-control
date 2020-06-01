c48ab7603743e6963d11932e0b58b3895900e1b9
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-06-01 20:13:03 +0300

    GET location by name

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,8 @@
 const noteRoutes = require('./note_routes');
+const locationsRoutes = require('./locations_routes');
+
 module.exports = function(app, db) {
   noteRoutes(app, db.db(`notes`));
+  locationsRoutes(app, db.db(`manuliatina`));
   // Тут, позже, будут и другие обработчики маршрутов 
 };
---
+++ app/routes/locations_routes.js
@@ -0,0 +1,66 @@
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {
+  // app.get('/locations/:id', (req, res) => {
+  //   console.log('req.params.id', req.params.id);
+  //   const id = req.params.id;
+  //   const details = { '_id': new ObjectID(id) };
+  //   db.collection('notes').findOne(details, (err, item) => {
+  //     if (err) {
+  //       res.send({'error':'An error has occurred'});
+  //     } else {
+  //       res.send(item);
+  //     } 
+  //   });
+  // });
+
+  
+  app.get('/locations/:name', (req, res) => {
+    console.log('req.params.name', req.params.name);
+    const name = req.params.name;
+    const details = { 'name': name };
+    db.collection('locations').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      } 
+    });
+  });
+
+  app.post('/notes', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Note ' + id + ' deleted!');
+      } 
+    });
+  });
+
+  app.put ('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(note);
+      } 
+    });
+  });
+};
