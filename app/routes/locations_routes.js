9920b16d5d9ac47e1136a981203c1dc0a438d322
Maxim Belyakov <58391051+maxim-belyakov@users.noreply.github.com>
2020-06-02 16:08:09 +0300

    get locataionS by name

--- app/routes/locations_routes.js
+++ app/routes/locations_routes.js
@@ -28,6 +28,49 @@ module.exports = function(app, db) {
     });
   });
 
+  // Function to handle the root path
+  // app.get('/', async function(req, res) {
+
+  //   // Access the provided 'page' and 'limt' query parameters
+  //   let page = req.query.page;
+  //   let limit = req.query.limit;
+
+  //   let articles = await Article.findAll().paginate({page: page, limit: limit}).exec();
+
+  //   // Return the articles to the rendering engine
+  //   res.render('index', {
+  //       articles: articles
+  //   });
+  // });
+
+  app.get('/locations', (req, res) => {
+    // const details = { 'name': name };
+
+    // { tags: ["red", "blank"] }
+    // db.collection("locations").find( { tags: { $all: { $all: names } } ) 
+
+    console.log(`req.query.names ${req.query.names}`)
+
+    if (req.query.names) {
+      const names = req.query.names.split`,`;
+      db.collection("locations").find({ name: { $in: names }}).toArray((err, items) => {
+        if (err) res.send({'error':'An error has occurred'});
+        else res.send(items);
+      } ) 
+    } else {
+      db.collection("locations").find({}).toArray((err, item) => {
+        if (err) res.send({'error':'An error has occurred'});
+        else res.send(item);
+      }); 
+    }
+     
+  });
+
+
+
+
+  
+  
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const app            = express();
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
 
   require('./app/routes')(app, database);
+
   app.listen(port, () => {
     console.log('We are live on ' + port);
-  });          
-       
+  });
+
 })
